56e3bf6b0b39745435242d7867a41544481dfe1b
denodaeus <bobby.smith@gmail.com>
2018-08-27 02:00:14 -0400

    ditch styling, use panel to fill size appropriately

--- imports/ui/components/Ride/RideListing.js
+++ imports/ui/components/Ride/RideListing.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Grid, Row, Col, ListGroup, ListGroupItem, Image } from 'react-bootstrap';
+import { Panel, Row, Col, ListGroup, ListGroupItem, Image } from 'react-bootstrap';
 
 const StyledRideListing = styled.div`
   img {
     max-width: 220px;
-    margin-bottom: 20px;
-  }
-
-  div {
-    padding: 3px;
   }
 `;
 
@@ -18,40 +13,35 @@ const RideListing = ({
   image, name, age, car, date, price, time, city,
 }) => (
   <StyledRideListing>
-    <Grid>
-      <Row className="show-grid">
-        <Col xs={6} md={3}>
-          {image ? <Image src={image} alt="" /> : ''}
-        </Col>
-        <Col xs={6} md={3}>
-          <ListGroup>
-            <ListGroupItem>Name: {name}</ListGroupItem>
-            <ListGroupItem>Age: {age}</ListGroupItem>
-            <ListGroupItem>Car: {car}</ListGroupItem>
-          </ListGroup>
-        </Col>
-        <Col xs={6} md={3}>
-          <ListGroup>
-            <ListGroupItem>Date: {date}</ListGroupItem>
-            <ListGroupItem>Price: {price}</ListGroupItem>
-          </ListGroup>
-        </Col>
-        <Col xs={6} md={3}>
-          <ListGroup>
-            <ListGroupItem>Time: {time}</ListGroupItem>
-            <ListGroupItem>City: {city}</ListGroupItem>
-          </ListGroup>
-        </Col>
-      </Row>
-    </Grid>
+    <Panel>
+      <Panel.Body>
+        <Row className="show-grid">
+          <Col xs={6} md={3}>
+            {image ? <Image src={image} alt="" /> : ''}
+          </Col>
+          <Col xs={6} md={3}>
+            <ListGroup>
+              <ListGroupItem>Name: {name}</ListGroupItem>
+              <ListGroupItem>Age: {age}</ListGroupItem>
+              <ListGroupItem>Car: {car}</ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col xs={6} md={3}>
+            <ListGroup>
+              <ListGroupItem>Date: {date}</ListGroupItem>
+              <ListGroupItem>Price: {price}</ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col xs={6} md={3}>
+            <ListGroup>
+              <ListGroupItem>Time: {time}</ListGroupItem>
+              <ListGroupItem>City: {city}</ListGroupItem>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Panel.Body>
+    </Panel>
   </StyledRideListing>
-  // <StyledRideListing>
-  //   {image ? <img src={image} alt={title} /> : ''}
-  //   {icon ? <Icon iconStyle={icon.style} icon={icon.symbol} /> : ''}
-  //   <h4>{title}</h4>
-  //   <p>{subtitle}</p>
-  //   {action ? <Button bsStyle={action.style || 'success'} onClick={action.onClick}>{action.label}</Button> : ''}
-  // </StyledRideListing>
 );
 
 RideListing.defaultProps = {
